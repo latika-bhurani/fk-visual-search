6bfd62dec24662ae3bc411e9c10ad57440c5555d
Sudhanshu Gupta <sudhanshu.gupta090@gmail.com>
2018-10-29 12:19:05 -0500

    Delete sample_app.py

--- flask-apps/sample_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-# instance of the flask class
-# use the __ name __ here if using a single module
-app = Flask(__name__)
-
-@app.route('/sample')
-def running():
-	return 'Flask is running'
